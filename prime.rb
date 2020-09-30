575e666a666af888436220d242467624c034d788
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-09-30 02:17:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 def prime?(num)
   n = 2
   while n < num
-    return false if num % n == 0 || num <= 0     
+    if num % n == 0 
+    elsif num <= 0 
+  else
+    false 
+#    return false if num % n == 0 || num <= 0     
     n += 1
   end
   true
