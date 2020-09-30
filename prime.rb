dfca0e5c923ca0c7d8d6cdc4b6799408d0b2d661
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-09-30 02:19:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(num)
   n = 2
-  while n < num {
+  while n < num 
     if num % n == 0 
       return true 
     elsif num <= 0 
       return true 
   else
-    return false }
+    return false 
 #    return false if num % n == 0 || num <= 0     
     n += 1
   end
